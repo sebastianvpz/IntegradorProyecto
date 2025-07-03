b9106b20bc75dac53f9c020a07ed8c72a85708e9
Eduardo Sanchez <u22205282@utp.edu.pe>
2025-07-03 13:11:46 -0500

    se añadio la vista del comnezal, falta conexion con base de datos

---
+++ src/main/java/com/utp/integradorspringboot/ComensalController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.utp.integradorspringboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Eduardo Sánchez
+ */
+@Controller
+public class ComensalController {
+    @GetMapping("/vistacomensal")
+    public String mostrarvistaComensal() {
+        return "vistacomensal"; // sin extensión .html
+    }
+}
